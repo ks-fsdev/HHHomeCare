e9c619b1d16547e0e6ada8fac7ed511e77032188
Khushi Srivastava <sweetkhushi2000@gmail.com>
2026-03-09 16:23:46 +0530

    db connection established

---
+++ hhhomecare/src/app/lib/db.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI!;
+
+if (!MONGODB_URI) {
+  throw new Error("Please define the MONGODB_URI environment variable");
+}
+
+let cached = (global as any).mongoose || { conn: null, promise: null };
+
+export async function connectDB() {
+  if (cached.conn) {
+    console.log("db still connected");
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    console.log("Connecting to db...");
+    cached.promise = mongoose.connect(MONGODB_URI).then((m) => {
+      console.log("db connected");
+      return m;
+    });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
